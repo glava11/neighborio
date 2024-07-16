6af925de9c13250ca99017f573b699f5c21199ef
drazen <glava11@gmail.com>
2024-07-16 23:55:11 +0200

    FE basic layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import Head from 'next/head';
 import * as React from 'react';
 import '@/lib/env';
 
-import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
 
 /**
  * SVGR Support
@@ -18,52 +16,123 @@ import UnstyledLink from '@/components/links/UnstyledLink';
  */
 import Logo from '~/svg/Logo.svg';
 
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 export default function HomePage() {
   return (
     <main>
       <Head>
-        <title>Hi</title>
+        <title>neighborio</title>
       </Head>
       <section className='bg-white'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-          <Logo className='w-16' />
-          <h1 className='mt-4'>Next.js + Tailwind CSS + TypeScript Starter</h1>
-          <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
-          </p>
-          <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
-            </ArrowLink>
-          </p>
+          <div className='flex flex-col items-center justify-center py-12 text-center'>
+            <Logo className='w-10' />
+            <h1>neighborio</h1>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <h1 className='mt-4'>Find your closest countries</h1>
+          {/* <p className='mt-2 text-sm text-gray-800'>
+            Find your closest countries{' '}
+          </p> */}
+
+          <form action='' className='mt-6 max-w-md mx-auto'>
+            {/* <input type='search' name='search' id='' /> */}
+            <label
+              htmlFor='default-search'
+              className='mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white'
+            >
+              Search
+            </label>
+            <div className='relative'>
+              <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
+                <svg
+                  className='w-4 h-4 text-gray-500 dark:text-gray-400'
+                  aria-hidden='true'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 20 20'
+                >
+                  <path
+                    stroke='currentColor'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
+                  />
+                </svg>
+              </div>
+              <input
+                type='search'
+                id='default-search'
+                className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                placeholder='Search your neighbors...'
+                required
+              />
+
+              <div
+                className='dropdown absolute z-10 mt-2 w-full origin-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+                role='menu'
+                aria-orientation='vertical'
+                aria-labelledby='menu-button'
+                tabIndex={-1}
+              >
+                <div className='py-1' role='none'>
+                  {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
+                  <a
+                    href='#'
+                    className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                    role='menuitem'
+                    tabIndex={-1}
+                    id='menu-item-0'
+                  >
+                    Account settings
+                  </a>
+                  <a
+                    href='#'
+                    className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                    role='menuitem'
+                    tabIndex={-1}
+                    id='menu-item-1'
+                  >
+                    Support
+                  </a>
+                  <a
+                    href='#'
+                    className='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                    role='menuitem'
+                    tabIndex={-1}
+                    id='menu-item-2'
+                  >
+                    License
+                  </a>
+                </div>
+              </div>
+            </div>
+          </form>
+
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
 
           <ButtonLink className='mt-6' href='/components' variant='light'>
             See all components
           </ButtonLink>
 
-          <UnstyledLink
-            href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-            className='mt-4'
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              width='92'
-              height='32'
-              src='https://vercel.com/button'
-              alt='Deploy with Vercel'
-            />
-          </UnstyledLink>
-
           <footer className='absolute bottom-2 text-gray-700'>
-            © {new Date().getFullYear()} By{' '}
-            <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-              Theodorus Clarence
-            </UnderlineLink>
+            © {new Date().getFullYear()}{' '}
+            <UnderlineLink href='https://neighbor.io'>neighborio</UnderlineLink>
           </footer>
         </div>
       </section>
